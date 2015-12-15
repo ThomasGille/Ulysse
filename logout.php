ed2ecf71fdbc11fcfaf32d168876471963316063
ThomasGille <totogille@gmail.com>
2015-12-15 16:27:34 +0100

    Balbutiement

---
+++ logout.php
@@ -0,0 +1,20 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+    // On démarre la session
+    session_start ();
+
+    // On détruit les variables de notre session
+    session_unset ();
+
+    // On détruit notre session
+    session_destroy ();
+
+    // On redirige le visiteur vers la page d'accueil
+    header("Location:./index.php");
+
