5712f33a03e2ee5389454bcd797f4376c0487e9c
ThomasGille <totogille@gmail.com>
2016-01-06 17:00:15 +0100

    Création et stockage Question + Réponses

--- CreaQuestion.php
+++ CreaQuestion.php
@@ -15,12 +15,20 @@ and open the template in the editor.
         include "Var.php";
         include "Menu.php";
         include "connectDB.php";
-        
-        
         ?>
         <form name="qcm" method="post" action="use_creaQuestion.php">
 
-            Entrez un énoncé : <input type="text" name="nom" value=""  required>
+            Entrez un énoncé : <input type="text" name="question" value="" size="50" required>
+            <br/><br/>
+            a : <input type="text" name="rep1" value=""  required>
+            True? <input type="checkbox" name="cha"  /><br/>
+            b : <input type="text" name="rep2" value=""  required>
+            True? <input type="checkbox" name="chb"  /><br/>
+            c : <input type="text" name="rep3" value=""  required>
+            True? <input type="checkbox" name="chc"  /><br/>
+            d : <input type="text" name="rep4" value=""  required>
+            True? <input type="checkbox" name="chd"  /><br/>
+            
             <br/>
             <br />
             <input type="submit" name="valider" value="Valider" />
---
+++ use_creaQuestion.php
@@ -0,0 +1,53 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+include "Var.php";
+include "connectDB.php";
+
+///////////// insertion question
+$sql = "INSERT INTO question VALUES (NULL, \"" . $_SESSION["idQCM"] . "\", \"" . $_POST["question"] . "\");";
+//echo $sql;
+
+if (mysqli_query($link, $sql)) {
+    // On redirige le visiteur vers la page création de question
+    echo 'question insérée <br />';
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($link);
+}
+
+////////////////recup id question
+$sql = "SELECT idQuestion as id FROM `question` where idQCM like \"" . $_SESSION["idQCM"] . "\" ORDER BY `question`.`idQuestion` DESC limit 1";
+//echo $sql;
+// on envoie la requête
+$req = mysqli_query($link, $sql) or die('Erreur SQL !<br>' . $sql . '<br>' . mysql_error());
+// on recupere la ligne 0 du resultat dans $data
+$data = mysqli_fetch_assoc($req);
+//print_r($data2);
+$idQuestion = $data['id'];
+echo "ID question = " . $idQuestion . "<br />";
+
+/////////// insertion réponses
+
+if(isset($_POST['cha']))$cha=1;else $cha=0;
+if(isset($_POST['chb']))$chb=1;else $chb=0;
+if(isset($_POST['chc']))$chc=1;else $chc=0;
+if(isset($_POST['chd']))$chd=1;else $chd=0;
+
+$sql = "INSERT INTO `ulysse`.`reponse` (`idReponse`, `idQuestion`, `enonceReponse`, `juste`) VALUES 
+        (NULL, " . $idQuestion . ", \"" . $_POST['rep1'] . "\"," . $cha . "), 
+        (NULL, " . $idQuestion . ", \"" . $_POST['rep2'] . "\"," . $chb . "),
+        (NULL, " . $idQuestion . ", \"" . $_POST['rep3'] . "\"," . $chc . "),
+        (NULL, " . $idQuestion . ", \"" . $_POST['rep4'] . "\"," . $chd . ");";
+//echo $sql;
+
+
+if (mysqli_query($link, $sql)) {
+    // On redirige le visiteur vers la page création de question
+    echo 'réponse insérée';
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($link);
+}
