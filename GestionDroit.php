e137b382ba88c187bbc51ad2406420720bbf4441
p1407041 <p1407041@IUTABGDIN106-07.univ-lyon1.fr>
2016-01-15 12:34:11 +0100

    Modification du menu en fonction de la session, suppression de "menuEleve", "menuProf", "InscriptionHTML" ; ajout de "Login"

--- GestionDroit.php
+++ GestionDroit.php
@@ -20,52 +20,55 @@ $pswd = $_POST['mdp'];
 
 $Crypt = Cryptage($pswd);
 
-if ($Crypt!= null){
-    
-    // 1. Personne dans BD ?? 
-    $requete="SELECT idPersonne FROM personne WHERE idPersonne = $idPers";
-    $reqID = mysqli_query($link, $requete) or die('Erreur SQL !<br>'.$requete.'<br>'.mysql_error());
-    $idPers = mysqli_fetch_array($reqID);
 
-    if ($idPers[0] == null){
-        echo "Erreur : personne inexistante / ID erroné";
-    }else{
-        // si etape 1. vrai : 
-        // 
-        // 2. Récupération Mdp de la personne
-        $requete="SELECT Mdp FROM ulysse.mdp WHERE idPersonne = $idPers[0] ";
-        $MDPHash = mysqli_query($link, $requete) or die('Erreur SQL !<br>'.$requete.'<br>'.mysql_error());
-        $pswd = mysqli_fetch_array($MDPHash);
-        
-        
 
-        // 4. Verification Password
-        if ($pswd[0]== $Crypt){
-            //5. Verification si Personne est prof ou élève
-            $requete="SELECT admin FROM personne WHERE idPersonne = $idPers[0] ";
-            $req = mysqli_query($link, $requete) or die('Erreur SQL !<br>'.$requete.'<br>'.mysql_error());
-            $admin = $pswd = mysqli_fetch_array($req);
-            
-           // 6. Recuperation de son nom
-			$requete="SELECT prenom FROM $dbname.personne WHERE idPersonne = $idPers[0]";
-			$reqID = mysqli_query($link, $requete) or die('Erreur SQL !<br>'.$requete.'<br>'.mysql_error());
-			$nomPers = mysqli_fetch_array($reqID);
-	
-            if ($admin[0] == '1'){
-                Session($idPers[0], $nomPers[0]);
-								
-                header('Location: index.php');      
+    if ($Crypt!= null){
 
-            }
-            else{
-                Session($idPers[0],$nomPers[0]);
-				
-                header('Location: index.php');      
-            }
-          
+        // 1. Personne dans BD ?? 
+        $requete="SELECT idPersonne FROM personne WHERE idPersonne = $idPers";
+        $reqID = mysqli_query($link, $requete) or die('Erreur SQL !<br>'.$requete.'<br>'.mysql_error());
+        $idPers = mysqli_fetch_array($reqID);
+
+        if ($idPers[0] == null){
+            echo "Erreur : personne inexistante / ID erroné";
         }else{
-            echo "Erreur : MDP incorrect";
+            // si etape 1. vrai : 
+            // 
+            // 2. Récupération Mdp de la personne
+            $requete="SELECT Mdp FROM ulysse.mdp WHERE idPersonne = $idPers[0] ";
+            $MDPHash = mysqli_query($link, $requete) or die('Erreur SQL !<br>'.$requete.'<br>'.mysql_error());
+            $pswd = mysqli_fetch_array($MDPHash);
+
+
+
+            // 4. Verification Password
+            if ($pswd[0]== $Crypt){
+                //5. Verification si Personne est prof ou élève
+                $requete="SELECT admin FROM personne WHERE idPersonne = $idPers[0] ";
+                $req = mysqli_query($link, $requete) or die('Erreur SQL !<br>'.$requete.'<br>'.mysql_error());
+                $admin = $pswd = mysqli_fetch_array($req);
+
+               // 6. Recuperation de son nom
+                            $requete="SELECT prenom FROM $dbname.personne WHERE idPersonne = $idPers[0]";
+                            $reqID = mysqli_query($link, $requete) or die('Erreur SQL !<br>'.$requete.'<br>'.mysql_error());
+                            $nomPers = mysqli_fetch_array($reqID);
+
+                if ($admin[0] == '1'){
+                    Session($idPers[0], $nomPers[0]);
+
+                    header('Location: index.php');      
+
+                }
+                else{
+                    Session($idPers[0],$nomPers[0]);
+
+                    header('Location: index.php');      
+                }
+
+            }else{
+                echo "Erreur : MDP incorrect";
+            }
         }
-    }
-  }
+      }
+
 
