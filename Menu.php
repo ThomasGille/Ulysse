5395a15405441242b78fd2f8c275bbd2cb369c78
ThomasGille <totogille@gmail.com>
2016-01-18 11:13:46 +0100

    Ajout du calcul de la note

--- Menu.php
+++ Menu.php
@@ -18,51 +18,51 @@ and open the template in the editor.
    
         
 		
-		<?php
+<?php
 
-			if (isset($_SESSION ["idPersonne"])){
-				if ($_SESSION ["idPersonne"] >=1000){
-					?>
-					<html> 
-						<ul id="menu-demo2">
-							<li><a href="#">Accueil</a></li>
-							<li><a href="#">QCM</a>
-								<ul>
-									<li><a href="CreaQCM.php">Création</a></li>
-									<li><a href="#">Consultation</a></li></li>
-									
-								</ul>
-							<li><a href="Logout.php">Logout</a></li>
-							<li><a href="index.php"><?php echo $_SESSION['prenomPersonne'] ?></a></li>
-						</ul>
-					</html>
-				<?php
-				}
-				else{
-				?>
-				<html>
-					<ul id="menu-demo2">
-						<li><a href="#">Accueil</a></li>
-						<li><a href="repondreQCM.php">Répondre QCM</a></li>
-						<li><a href="Logout.php">Logout</a></li>
-						<li><a href="index.php"><?php echo  $_SESSION['prenomPersonne'] ?></a></li>
-					</ul>
-					
-				</html>
-			<?php
-				}
-			}
-			else{
-				?>
-				<html>
-					<ul id="menu-demo2">
-						<li><a href="index.php">Acceuil</a><br/></li>
-						<li><a href="Login.php">Login</a><br/></li>
-					</ul>
-				</html>
-			<?php
-			}			
-			?>
+        if (isset($_SESSION ["idPersonne"])){
+                if ($_SESSION ["idPersonne"] >=1000){
+                        ?>
+                        <html> 
+                                <ul id="menu-demo2">
+                                        <li><a href="#">Accueil</a></li>
+                                        <li><a href="#">QCM</a>
+                                                <ul>
+                                                        <li><a href="CreaQCM.php">Création</a></li>
+                                                        <li><a href="listeQCM.php">Consultation</a></li></li>
+
+                                                </ul>
+                                        <li><a href="Logout.php">Logout</a></li>
+                                        <li><a href="index.php"><?php echo $_SESSION['prenomPersonne'] ?></a></li>
+                                </ul>
+                        </html>
+                <?php
+                }
+                else{
+                ?>
+                <html>
+                        <ul id="menu-demo2">
+                                <li><a href="#">Accueil</a></li>
+                                <li><a href="repondreQCM.php">Répondre QCM</a></li>
+                                <li><a href="Logout.php">Logout</a></li>
+                                <li><a href="index.php"><?php echo  $_SESSION['prenomPersonne'] ?></a></li>
+                        </ul>
+
+                </html>
+        <?php
+                }
+        }
+        else{
+                ?>
+                <html>
+                        <ul id="menu-demo2">
+                                <li><a href="index.php">Acceuil</a><br/></li>
+                                <li><a href="Login.php">Login</a><br/></li>
+                        </ul>
+                </html>
+        <?php
+        }			
+        ?>
 		
         
 		
